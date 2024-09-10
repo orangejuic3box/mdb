329f2a3a554f391a4d5af58186c83a17d0b58a59
Maggie Lacson <58317354+orangejuic3box@users.noreply.github.com>
2024-09-10 17:04:19 -0600

    update a1

--- assignment1/a1.py
+++ assignment1/a1.py
@@ -15,10 +15,14 @@ class CommandInterface:
             "game" : self.game,
             "show" : self.show,
             "play" : self.play,
+            "place" : self.place,
             "legal" : self.legal,
             "genmove" : self.genmove,
             "winner" : self.winner
         }
+        self.grid = None
+        self.height = None
+        self.weight = None
         # do i set empty self.grid, self.height, self.width??
 
     # Convert a raw string to a command and a list of arguments
@@ -74,6 +78,8 @@ class CommandInterface:
             # If valid grid size, create grid
             if 1 <= width <= 20 and 1 <= height <= 20:
                 self.grid = [["." for _ in range(width)] for _ in range(height)]
+                self.height = height
+                self.width = width
                 return True
 
             else:
@@ -93,7 +99,8 @@ class CommandInterface:
             return True
         
         # Raise exception if no grid exists
-        except:
+        except Exception as e:
+            print(e)
             print("Error: No game exists.")
             return False
     
@@ -101,17 +108,70 @@ class CommandInterface:
         raise NotImplementedError("This command is not yet implemented.")
         return True
     
+    def place(self,args):
+        ''' Helper Function for legal purposes'''
+        # just places the digit at x y in the grid
+        x = int(args[0])
+        y = int(args[1])
+        digit = args[2]
+
+        self.grid[x][y] = digit
+        # print(self.grid)
+        print("placed")
+        return True
+    
+    
     def legal(self, args):
-        raise NotImplementedError("This command is not yet implemented.")
+        # raise NotImplementedError("This command is not yet implemented.")
         # Check if this move (in the same format as in play) is legal. 
         # Answer yes or no. 
         # The command status is = 1.
         
         # Triple contraint: no 3 consecutive digits in ROW or COLUMN
-        # Balance constraint
+        # Balance constraint: the max number of playable digits is half the size of its ROW and COLUMN (rounded up)
 
+        try:
+            print("converting arguments")
+            x = int(args[0])
+            y = int(args[1])
+            digit = int(args[2])
+            print("converted")
+            # Goes to the row and checks both constraints
+            row = self.grid[y]
+            for i in range(self.width):
+                # print("i is", i)
+                # Case: grid width is 3 or smaller
+                if self.width <= 3:
+                    if self.width == 1:
+                        # print("width is 1")
+                        print(row[i])
+                    if self.width == 2:
+                        # print("width is 2")
+                        print(row[i], row[i+1])
+                    if self.width == 3:
+                        # print("width is 3")
+                        print(row[i], row[i+1], row[i+2])
+                    break
+                # Case: grid width bigger than 3
+                if i < self.width-2: # need to test this against width 1, 2, 3
+                    # print(i, width-2)
+                    consec = [row[i], row[i+1], row[i+2]]
+                    # print(row[i], row[i+1], row[i+2])
+                    print(consec)
 
-        return True
+                else:
+                    print("Width is 0")
+                    break
+            # Goes to the column and checks both constraints
+            return True
+        except TypeError as e:
+            print(e)
+            print("Error: Game does not exist.")
+            return False
+        except Exception as e:
+            print(e)
+            print("Error: Invalid arguments for legal command.\nMust be in format: x y digit")
+            return False
     
     def genmove(self, args):
         raise NotImplementedError("This command is not yet implemented.")
